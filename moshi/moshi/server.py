600fe3544b63d685bc69223c4af05127efa27555
Sihan <dong70@byu.edu>
2026-03-16 15:40:34 -0600

    GPU kernel自动优化

--- moshi/moshi/server.py
+++ moshi/moshi/server.py
@@ -73,7 +73,7 @@ def seed_all(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.backends.cudnn.deterministic = False
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.benchmark = True
 
 
 def wrap_with_system_tags(text: str) -> str:
